39c4e8aa974e9ae4a2ab202a8717093be53c9ad4
Natan <hc.natan@gmail.com>
2021-05-04 08:43:12 -0300

    Projeto criado

---
+++ src/main/java/br/com/exemple/MccourseApplication.java
@@ -0,0 +1,13 @@
+package br.com.exemple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MccourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MccourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/exemple/MccourseApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.exemple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MccourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
